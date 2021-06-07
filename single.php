fce65222e48c5956d85c2103fb0129aa9c00e626
ande19i7 <ande19i7@stud.kea.dk>
2021-06-07 16:50:46 +0200

    signle fix

--- single.php
+++ single.php
@@ -40,7 +40,7 @@ get_header(); ?>
         <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     </head>
 
-    <section id="produkt-oversigt">
+    <section>
 
         <button class="back-button">Back</button>
         <div id="loadingscreen">
@@ -48,7 +48,7 @@ get_header(); ?>
             <div id="spacer">
             </div>
         </div>
-        <article>
+        <article id="produkt-oversigt">
             <div class="row">
                 <div class="column">
                     <div class="storebillede">
